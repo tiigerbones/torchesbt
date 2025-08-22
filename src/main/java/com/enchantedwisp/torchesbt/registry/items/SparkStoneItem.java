cd240d3cf90ba399f623aa1cca836e42323e953b
tiigerbones <tiiger.b@outlook.com>
2025-08-22 13:36:08 -0600

    added new item for early game ignition

---
+++ src/main/java/com/enchantedwisp/torchesbt/registry/items/SparkStoneItem.java
@@ -0,0 +1,9 @@
+package com.enchantedwisp.torchesbt.registry.items;
+
+import net.minecraft.item.Item;
+
+public class SparkStoneItem extends Item {
+    public SparkStoneItem(Settings settings) {
+        super(settings);
+    }
+}
