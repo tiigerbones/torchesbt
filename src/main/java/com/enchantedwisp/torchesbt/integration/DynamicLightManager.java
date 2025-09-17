d0cd8f35db6a81dcb0b24aea6d5307b5dfafff9b
tiigerbones <tiiger.b@outlook.com>
2025-09-17 16:34:51 -0600

    redundant helper removed

--- src/main/java/com/enchantedwisp/torchesbt/integration/DynamicLightManager.java
+++
@@ -1,9 +0,0 @@
-package com.enchantedwisp.torchesbt.integration;
-
-import com.enchantedwisp.torchesbt.util.ConfigCache;
-
-public class DynamicLightManager {
-    public static boolean isDynamicLightsEnabled() {
-        return ConfigCache.isDynamicLightsEnabled();
-    }
-}
